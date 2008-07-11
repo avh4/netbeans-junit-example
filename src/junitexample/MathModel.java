f1f19e7d8166bee1bffa9c3282cdbe2e72a5b97c
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-11 13:39:25 -0700

    New Java Class.

---
+++ src/junitexample/MathModel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package junitexample;
+
+/**
+ *
+ * @author avh4
+ */
+public class MathModel {
+    
+    public int add(int a, int b) {
+        return 0;
+    }
+    
+    public int subtract(int a, int b) {
+        return 0;
+    }
+
+}
