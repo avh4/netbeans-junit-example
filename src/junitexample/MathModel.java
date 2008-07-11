e6a8c88686cea473e566b22138c606a65996eb1f
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-11 13:49:21 -0700

    Tests pass.

--- src/junitexample/MathModel.java
+++ src/junitexample/MathModel.java
@@ -12,11 +12,11 @@ package junitexample;
 public class MathModel {
     
     public int add(int a, int b) {
-        return 0;
+        return a + b;
     }
     
     public int subtract(int a, int b) {
-        return 0;
+        return a - b;
     }
 
 }
